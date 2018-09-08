db25fdaaaad236e82b99c3d9cadf91d596678f28
Gregory Dillon <gregorydillon@gmail.com>
2018-09-08 01:03:22 +0000

    update scripts for node, tmux and yarn

--- packages/tmux.sh
+++ packages/tmux.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-sudo apt-get remove tmux -y
-sudo apt-get install build-essential libevent-dev ncurses-dev -y
+apt remove tmux -y
+apt install build-essential libevent-dev ncurses-dev -y
 
 pushd /tmp/
 
@@ -9,6 +9,6 @@ wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
 tar -xzf tmux-2.6.tar.gz
 cd tmux-2.6
 ./configure && make
-sudo make install
+make install
 
 popd
